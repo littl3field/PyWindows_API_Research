3fb9793eaa2546f6b640a0ab690495421072a7db
Ryan Littlefield <ryan.j.littlefield@gmail.com>
2019-06-20 23:04:57 +0100

    Add files via upload

---
+++ UndocCall.py
@@ -0,0 +1,32 @@
+import ctypes
+from ctypes.wintypes import DWORD, HANDLE, LPWSTR
+
+k_handle = ctypes.WinDLL("Kernel32.dll")
+d_handle = ctypes.WinDLL("DNSAPI.dll")
+
+class DNS_CACHE_ENTRY(ctypes.Structure):
+    _fields_ = [
+        ("pNext", HANDLE),
+        ("recName", LPWSTR),
+        ("wType", DWORD),
+        ("wDataLength", DWORD),
+        ("dwFlags", DWORD),
+    ]
+
+print("INFO: Pulling DNS Cache Data from System")
+DNS_Entry = DNS_CACHE_ENTRY()
+DNS_Entry.wDataLength = 1024
+response = d_handle.DnsGetCacheDataTable(ctypes.byref(DNS_Entry))
+
+if response == 0:
+    print("Error code: {0}".format(k_handle.GetLastError()))
+
+DNS_Entry = ctypes.cast(DNS_Entry.pNext, ctypes.POINTER(DNS_CACHE_ENTRY))
+
+while True:
+    try:
+        print("DNS Entry {0} - Type {1}".format(DNS_Entry.contents.recName, DNS_Entry.contents.wType))
+        DNS_Entry = ctypes.cast(DNS_Entry.contents.pNext, ctypes.pointer(DNS_CACHE_ENTRY))
+
+    except:
+        break
